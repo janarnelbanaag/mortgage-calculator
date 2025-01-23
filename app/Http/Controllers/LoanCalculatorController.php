f8eb5e8ddfa92d3b3841b76adb8a8e6381add13b
Jan Arnel Banaag <jhonarnel476@gmail.com>
2025-01-23 14:08:59 +0800

    temp commit

---
+++ app/Http/Controllers/LoanCalculatorController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoanCalculatorRequest;
+use App\Services\LoanCalculatorService;
+use App\Models\LoanAmortizationSchedule;
+
+class LoanCalculatorController extends Controller
+{
+    private $calculatorService;
+
+    public function __construct(LoanCalculatorService $calculatorService)
+    {
+        $this->calculatorService = $calculatorService;
+    }
+
+    public function calculate(LoanCalculatorRequest $request)
+    {
+        $result = $this->calculatorService->generateAmortizationSchedule(
+            $request->loan_amount,
+            $request->annual_interest_rate,
+            $request->loan_term_years
+        );
+
+        foreach ($result['schedule'] as $entry) {
+            LoanAmortizationSchedule::create([
+                'month_number' => $entry['month'],
+                'starting_balance' => $entry['starting_balance'],
+                'monthly_payment' => $entry['monthly_payment'],
+                'principal_component' => $entry['principal_payment'],
+                'interest_component' => $entry['interest_payment'],
+                'ending_balance' => $entry['ending_balance']
+            ]);
+        }
+
+        return response()->json($result);
+    }
+}
